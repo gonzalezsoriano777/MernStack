d3f3eb3a3bc4b880544b700ca8fdb1d0bf273e6d
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-11-06 00:15:09 +0000

    Intro

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Notes
+
+// What this is...
+/*
+
+This is to learn MernStack 
+To build and deploy a full stack application using MERN Stack
+
+*/ 
+
+
+// Main Technologies Used..
+/*
+
+ES6+ JS .. New to me for JS // For arrow functions and destructering
+Node.js/ Express // Will be used for the Backend running the application..
+MongoDB // For the database
+React and Redux // Front end FrameWork .. Redux for the Application State
+Twitter Bootstrap 4 .. New to me // Used for the UI
+Heroku and Git for Deployment .. New to me // Used for Deployment
+
+*/
