355804e7da763cc12fbe8f0a6c8d1fa0bf7bbb12
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-11-14 01:37:25 +0000

    Notes

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ Heroku and Git for Deployment .. New to me // Used for Deployment
 
 */
 
-
+const DevConnector = {};
   DevConnector
 // What will do with the DevConnector ..
 /*
@@ -31,3 +31,54 @@ Small social network with authentication, profiles, dashboard, posts
 */
 
 // Res.json is identical to res.send but it outputs a json.. serves json
+
+const Bcrypt = {};
+ Bcrypt
+/*
+
+Bcrypt - Password hashing function designed by Niels Provos and David Mazieres
+It's based of the Cipher 'Blowfish'
+Helps prevent critical identity data from falling into the wrong hands.
+Bcrypt function is the default password hash algorithm for OpenBSD and other systems including some Linux distributions such as SUSE Linux
+
+
+genSalt() - method takes an optional parameter that determines the computational complexity of the hashing
+
+salt - the salt to hash with (perhaps generated using BCrypt.gensalt)
+*/
+
+const Status = {}
+    Status 
+/*
+
+100 - Continue, everything so far is OK
+101 - Switching Protocol, Upgrade requet header for the client, indicates the server protocol is switching
+102 - Pocessing (WebDAV), Server has recieved and processing the request
+200 - OK, everything is working just fine and everything has executed correctly.. request has SUCCEEDED!
+201 - Created , new source has been added to the data itself, typically this revolves around a POST request
+202 - Accepted, a request has been recieved but not yet executed
+204 - No Content - No Content to be sent for the request
+205 - Reset Content, Responce code is sent after accomplishing request to tell user agent reset document view which sent this request
+206 - Partial Content, Responce code used because of range headers sent by the client to seperate download into mutiple streams
+400 - Bad request, server could not understand what is being requested
+401 - Unauthorized - Client must authenticate itself to get the requested responce
+402 - Payment Required
+403 - Forbidden, Client does not have access to the rights of the content
+404 - Not found, what ever is being requested could not be found
+426 - Update Required
+500 - Internal Server Error, Server has encountered an error, doesn't know how to handle it
+501 - Not Implemented - Request method is not supported by the server
+511 - Network Authentication Required
+
+*/
+
+const jwt = {};
+    
+/*
+
+When you develop an API, most of the times you'll need part or all of it's endpoints to require authentication
+- Passport strategy for authenticating with JSON Web Token. 
+Lets you authenticate end points using JSON web token
+Used for security, gives you a token in which authorizes you to log in
+
+*/
