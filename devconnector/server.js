e213e2b8bb6386ef7759a288ed61987dbb608e00
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-11-17 18:54:41 +0000

    Minor issues

--- devconnector/server.js
+++ devconnector/server.js
@@ -22,6 +22,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+
+
 // Passport middleware
 app.use(passport.initialize());
 
@@ -36,3 +38,5 @@ app.use('/api/posts', post);
 const port = process.env.PORT || 8080; // To run Heroku using the port you must use 'process.env.PORT'
 
 app.listen(port, () => console.log(`Server running on port ${port}`)) // Using the back tics runs the ES6 for JS and you can run the server and call the port using the variable syntax .. which outputs whatever the port is
+
+module.exports = app;
