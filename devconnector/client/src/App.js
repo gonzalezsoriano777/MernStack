9ff0be7fdad1cbe8008bb6586e0cb188d1ffef7a
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-27 17:55:07 +0000

    Errors needed to be fix

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utile/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authAction';
+import { clearCurrentProfile } from './actions/profileActions';
+
+
 import { Provider } from 'react-redux';
 import store from './store';
+
 
 
 import Navbar from './components/layout/Navbar';
@@ -9,9 +16,33 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 
 import './App.css';
+
+
+
+// Check for token
+if (localStorage.jwtToken) {
+  
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 
 class App extends Component {
@@ -25,6 +56,7 @@ class App extends Component {
        <div className = "container">
          <Route exact path="/register" component={Register} />
          <Route exact path="/login" component={Login} />
+         <Route exact path="/dashboard" components={Dashboard} />
          </div>
        <Footer />
       </div>
--- devconnector/client/src/components/auth/Login.js
+++ devconnector/client/src/components/auth/Login.js
@@ -1,69 +1,99 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authAction';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Login extends Component {
-    constructor() {
-      super();
-      this.state = {
-          email: '',
-          password: '',
-          error: {}
-      };
-      this.onChange = this.onChange.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
     }
-    
-    onChange(e) {
-        this.setState({[e.target.name]: e.target.value })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
     }
-    
-    onSubmit(e){
-      e.preventDefault();
-      
-      const user = {
-          email: this.state.email,
-          password: this.state.password
-      };
-      
-      console.log(user);
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
-    
-    render(){
-        return (
-           <div className="login">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 m-auto">
-          <h1 className="display-4 text-center">Log In</h1>
-          <p className="lead text-center">Sign in to your DevConnector account</p>
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <input 
-              type="email" 
-              className="form-control form-control-lg" 
-              placeholder="Email Address" 
-              name="email" 
-              value = {this.state.email}
-              onChange={this.onChange}
-              />
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.loginUser(userData);
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
+
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
-            <div className="form-group">
-              <input 
-              type="password" 
-              className="form-control form-control-lg" 
-              placeholder="Password" 
-              name="password" 
-              value = {this.state.password}
-              onChange={this.onChange}
-              />
-            </div>
-            <input type="submit" className="btn btn-info btn-block mt-4" />
-          </form>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-        );
-    }
+    );
+  }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
