eaa430048f2174a8ccb4d3121b87a7b7aafac66d
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-27 03:19:01 +0000

    Fixing it, login can accept and make profile and logout

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utile/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authAction';
+
+
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -12,6 +18,27 @@ import Login from './components/auth/Login';
 
 
 import './App.css';
+
+// Check for token
+if(localStorage.jwtToken) {
+    // Set auth token, header auth
+    setAuthToken(localStorage.jwtToken);
+    // Decode token and get user info and exp
+    const decode = jwt_decode(localStorage.jwtToken);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decode));
+    
+    // Check for expired token
+    const currentTime = Date.now() / 1000;
+    if(decode.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());
+        // TODO: Clear current profile
+        // Redirect to login
+        window.location.href = '/login';
+    }
+}
+
 
 
 class App extends Component {
