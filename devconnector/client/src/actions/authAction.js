af405f5e35c76e58509224891043e3b68179ffea
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-28 03:17:56 +0000

    Errors will be resolved

--- devconnector/client/src/actions/authAction.js
+++ devconnector/client/src/actions/authAction.js
@@ -11,7 +11,7 @@ import { GET_ERRORS, SET_CURRENT_USER } from './types';
 export const registerUser = (userData, history) => dispatch => {
         axios
        .post('/api/users/register', userData)
-       .then(res => history.push('/login'))
+       .then(res => console.log(res.data))
        .catch(err => 
          dispatch({
          type: GET_ERRORS,
--- devconnector/client/src/components/layout/Navbar.js
+++ devconnector/client/src/components/layout/Navbar.js
@@ -1,57 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { logoutUser } from '../../actions/authAction';  
 
 class Navbar extends Component {
-  onLogoutClick(e){
-    e.preventDefault();
-    this.props.logoutUser();
-  }
-  
-  
-  
-    render(){
-      const { isAuthenticated, user } = this.props.auth;
-      
-      const authLinks = (
-           <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link 
-            className="nav-link" to="/register">
-            Sign Up
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">
-            Login
-            </Link>
-          </li>
-        </ul>
-        );
-        
-        const guestLinks = (
-           <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-             <a href="" 
-             onClick={this.onLogoutClick.bind(this)} 
-             className="nav-link">
-             
-             
-             <img 
-             className="rounded-circle"
-             src={user.avatar} 
-             alt={user.name} 
-             style={{ width: '25px', marginRight: '5px' }} 
-             title ="You must have gravatar connected to your email to display an image" 
-             />{' '} 
-             Logout
-             </a>
-          </li>
-        </ul>
-        );
-      
+    render() {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
     <div className="container">
@@ -71,7 +22,20 @@ class Navbar extends Component {
             </Link>
           </li>
         </ul>
-        {isAuthenticated ? authLinks : guestLinks}
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link 
+            className="nav-link" to="/register">
+            Sign Up
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+            Login
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
@@ -79,14 +43,134 @@ class Navbar extends Component {
  }
 }
 
-Navbar.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-}
+export default Navbar;
 
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { logoutUser } from '../../actions/authAction';  
+
+// class Navbar extends Component {
+//   onLogoutClick(e){
+//     e.preventDefault();
+//     this.props.logoutUser();
+//   }
+  
+  
+  
+//     render(){
+//       const { isAuthenticated, user } = this.props.auth;
+      
+//       const authLinks = (
+//           <ul className="navbar-nav ml-auto">
+//           <li className="nav-item">
+//             <Link 
+//             className="nav-link" to="/register">
+//             Sign Up
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/login">
+//             Login
+//             </Link>
+//           </li>
+//         </ul>
+//         );
+        
+//         const guestLinks = (
+//           <ul className="navbar-nav ml-auto">
+//           <li className="nav-item">
+//             <a href="#value" 
+//             onClick={this.onLogoutClick.bind(this)} 
+//             className="nav-link">
+             
+             
+//             <img 
+//             className="rounded-circle"
+//             src={user.avatar} 
+//             alt={user.name} 
+//             style={{ width: '25px', marginRight: '5px' }} 
+//             title ="You must have gravatar connected to your email to display an image" 
+//             />{' '} 
+//             Logout
+//             </a>
+//           </li>
+//         </ul>
+//         );
+      
+//     return (
+//         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+//     <div className="container">
+//       <Link className="navbar-brand" to="/">
+//       DevConnector
+//       </Link>
+//       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+
+//       <div className="collapse navbar-collapse" id="mobile-nav">
+//         <ul className="navbar-nav mr-auto">
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/profiles">
+//             {' '}
+//             Developers
+//             </Link>
+//           </li>
+//         </ul>
+//         {isAuthenticated ? authLinks : guestLinks}
+//       </div>
+//     </div>
+//   </nav>
+//   )
+// }
+// }
+
+// Navbar.propTypes = {
+//   logoutUser: PropTypes.func.isRequired,
+//   auth: PropTypes.object.isRequired
+// }
+
+
+// const mapStateToProps = (state) => ({
+//   auth: state.auth,
+// });
+
+// export default connect(mapStateToProps, { logoutUser })(Navbar);
