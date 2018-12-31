fa11a98900b6099cd1a566d1b1f66eb1ca128c3c
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-31 14:53:32 +0000

    Made a profile and able to add info for profile

--- devconnector/client/src/actions/profileActions.js
+++ devconnector/client/src/actions/profileActions.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 import { 
     GET_PROFILE, 
-    PROFILE_LOADING, 
+    PROFILE_LOADING,
+    GET_ERRORS,
     CLEAR_CURRENT_PROFILE 
     } from './types';
 
@@ -23,7 +24,20 @@ export const getCurrentProfile = () => dispatch => {
           payload: {}
       })
     );
-}
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => {
+        dispatch({ 
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    });
+};
 
 
 // Profile Loading
--- devconnector/client/src/components/create-profile/CreateProfile.js
+++ devconnector/client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import { createProfile } from '../../actions/profileActions'
 
 
 class CreateProfile extends Component {
@@ -33,11 +35,34 @@ class CreateProfile extends Component {
         
     }
     
+    componentWillRecieveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+    
     onSubmit(e) {
             e.preventDefault();
             
-            console.log('submit');
-        }
+      const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+  }
+    
+    this.props.createProfile(profileData, this.props.history);
+    
+}
         
     onChange(e) {
         this.setState({ [e.target.name]: e.target.value });
@@ -61,7 +86,7 @@ class CreateProfile extends Component {
                   error={errors.twitter}
                 />
                 
-            <InputGroup
+                <InputGroup
             placeholder="Facebook Page URL"
             name="facebook"
             icon="fab fa-facebook"
@@ -197,7 +222,9 @@ class CreateProfile extends Component {
                  /> 
                  
                  <div className="mb-3">
-                    <button onClick={() => {
+                    <button
+                    type="button"
+                    onClick={() => {
                         this.setState(prevState => ({
                         displaySocialInputs: !prevState.displaySocialInputs
                       }));
@@ -227,4 +254,4 @@ const mapStateToProps = (state) => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, {createProfile})(withRouter(CreateProfile));
